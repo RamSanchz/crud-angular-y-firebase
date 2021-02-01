ad01e1fc52c84a3c5aa61af971ac3b46fee784f0
Ramiro Sanchez Windows <dosek1996@gmail.com>
2021-02-01 08:28:13 -0600

    Crud Angular y firenbase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeroeComponent, HeroesComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/pages/heroe/heroe.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HeroeModel } from '../../models/heroe.model';
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+  styleUrls: ['./heroe.component.css'],
+})
+export class HeroeComponent implements OnInit {
+  heroe: HeroeModel = new HeroeModel();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  guardar(form: NgForm): void {
+    if (form.invalid) {
+      console.log('Formulario no valido');
+      return;
+    }
+
+    console.log(form);
+    console.log(this.heroe);
+  }
+}
